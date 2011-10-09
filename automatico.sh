4118c424267c9e7b1c7dae19e778421c929b0ab3
Glauco Roberto Munsberg <glaucomunsberg@gmail.com>
2011-10-08 23:18:42 -0300

    testefinal

--- automatico.sh
+++ automatico.sh
@@ -6,7 +6,7 @@ echo "- Carregando os arquivos...                     -"
 git add *.*
 echo "- Insira o nome do commit:                      -"
 read nome
-git commit -m '$nome'
+git commit -m $nome
 echo "- Commintando...                                -"
 git remote add origin git@github.com:glaucomunsberg/PHP.git
 git push origin master
