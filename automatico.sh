45735535f96db7561f0a1db10316ba97e185ff3c
Glauco Roberto Munsberg <glaucomunsberg@gmail.com>
2011-10-08 23:13:45 -0300

    $nome

--- automatico.sh
+++ automatico.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-echo "Commit automático"
+echo "-------------------------------------------------"
+echo "                 Commit automatic                "
+echo "-------------------------------------------------"
+echo "- Carregando os arquivos...                     -"
 git add *.*
-echo "Insira o nome do commit:"
+echo "- Insira o nome do commit:                      -"
 read nome
 git commit -m '$nome'
-echo "Commintando..."
+echo "- Commintando...                                -"
 git remote add origin git@github.com:glaucomunsberg/PHP.git
 git push origin master
-echo "Commitado!"
-
+echo "-                  Commitado!                   -"
+echo "-------------------------------------------------"
 
 
