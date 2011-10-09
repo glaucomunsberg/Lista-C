962e67dc9182175f4b5d5441cab823284672cd24
Glauco Roberto Munsberg <glaucomunsberg@gmail.com>
2011-10-09 00:33:20 -0300

    pronto

--- automatico.sh
+++ automatico.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 echo "-------------------------------------------------"
-echo "                 Commit automatic                "
+echo "                      GITHUB                     "
 echo "-------------------------------------------------"
-echo "- Deseja inserir algo no README?S/N             -"
-read opcao
-if [ "S" = "$opcao" ];
-then
-	gedit README
-	git add README
+echo "Commit ou Push?				      -"
+read coup
+if [ "C" = "$coup" ]; then
+	echo "- Deseja inserir algo no README?S/N             -"
+	read opcao
+	#
+	if [ "S" = "$opcao" ]; then
+		gedit README
+		git add README
+	fi
+	#
+	echo "- Carregando os arquivos...                     -"
+	git add *.*
+	echo "- Insira o nome do commit:                      -"
+	read nome
+	git commit -m $nome
+	echo "- Commintando...                                -"
+	git remote add origin git@github.com:glaucomunsberg/PHP.git
+	git push origin master
+	echo "-                  Commitado!                   -"
+	echo "-------------------------------------------------"
 else
-echo ""
+	echo "Push... 					      -"
+	git remote add origin git@github.com:glaucomunsberg/PHP.git
+	git push origin master
+	echo "-                  Push!	                      -"
+	echo "-------------------------------------------------"
 fi
-
-echo "- Carregando os arquivos...                     -"
-git add *.*
-echo "- Insira o nome do commit:                      -"
-read nome
-git commit -m $nome
-echo "- Commintando...                                -"
-git remote add origin git@github.com:glaucomunsberg/PHP.git
-git push origin master
-echo "-                  Commitado!                   -"
-echo "-------------------------------------------------"
-
-
